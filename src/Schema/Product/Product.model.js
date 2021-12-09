9d92298f575baa3329bc6c98a763a6b544997e70
tommaso merlini <tommaso.merlini.2004@gmail.com>
2021-12-09 16:27:07 +0100

    product uses status instead of isActive

--- src/Schema/Product/Product.model.js
+++ src/Schema/Product/Product.model.js
@@ -3,31 +3,31 @@ const mongoose = require("mongoose");
 mongoose.set("useCreateIndex", true);
 
 const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
-  images: [{ type: String, required: true }],
-  description: { type: String, required: false },
-  shopName: { type: String, required: true },
-  likes: { type: Number, required: true },
-  location: {
-    type: {
-      type: String,
-      required: true,
+    name: {
+        type: String,
+        required: true,
     },
-    coordinates: [{ type: Number, required: true }],
-  },
-  isActive: { type: Boolean, required: true },
-  shopID: { type: mongoose.Types.ObjectId, required: true },
+    price: {
+        type: Number,
+        required: true,
+    },
+    weight: {
+        type: Number,
+        required: true,
+    },
+    images: [{ type: String, required: true }],
+    description: { type: String, required: false },
+    shopName: { type: String, required: true },
+    likes: { type: Number, required: true },
+    location: {
+        type: {
+            type: String,
+            required: true,
+        },
+        coordinates: [{ type: Number, required: true }],
+    },
+    status: { type: String, required: true },
+    shopID: { type: mongoose.Types.ObjectId, required: true },
 });
 
 const Product = mongoose.model("product", ProductSchema);
