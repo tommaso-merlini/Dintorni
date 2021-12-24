53864156a5130af16ab4d49364b3a0b29521666e
tommaso merlini <tommaso.merlini.2004@gmail.com>
2021-12-24 15:11:24 +0100

    products query handles errors

--- src/GraphQL/Functions/Queries/Product/products.js
+++ src/GraphQL/Functions/Queries/Product/products.js
@@ -1,14 +1,27 @@
 const Product = require("../../../../Schema/Product/Product.model");
 const { GraphQLError } = require("graphql");
-
 const MongoFilter = require("../../../MongoFilter/MongoFilter");
-
 
 const products = async (_, { ids }, __, info) => {
     try {
+        const returnedIDs = [];
+        const unkownIDs = [];
         const filter = MongoFilter(info);
         const products = await Product.find({ _id: { $in: ids } }, filter);
+        products.map((product) => {
+            returnedIDs.push(JSON.stringify(product._id));
+        });
+
+        ids.map((id) => {
+            if (!returnedIDs.includes(JSON.stringify(id))) {
+                unkownIDs.push(id);
+            }
+        })
+
+        //TODO: return the unkown ids as errors
+
         return products;
+
     } catch (e) {
         console.log("somthing went wrong while searching for products");
         throw new GraphQLError(e.message);
