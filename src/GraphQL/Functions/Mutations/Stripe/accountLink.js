e77be1e65b6541d5f6c2b0baea571c1c21260db4
tommaso merlini <tommaso.merlini.2004@gmail.com>
2021-12-25 20:26:05 +0100

    fixed account link

--- src/GraphQL/Functions/Mutations/Stripe/accountLink.js
+++ src/GraphQL/Functions/Mutations/Stripe/accountLink.js
@@ -1,12 +1,24 @@
-const accountLink = async (_, { accountId }, { stripe }) => {
-  const accountLink = await stripe.accountLinks.create({
-    account: accountId,
-    refresh_url: "https://www.comune.perugia.it/",
-    return_url: "https://www.comune.terni.it/",
-    type: "account_onboarding",
-  });
+const { GraphQLError } = require("graphql");
 
-  return accountLink.url;
+
+
+
+
+const accountLink = async (_, { accountID }, { stripe }) => {
+    try {
+        const accountLink = await stripe.accountLinks.create({
+            account: accountID,
+            refresh_url: "https://www.comune.perugia.it/",
+            return_url: "https://www.comune.terni.it/",
+            type: "account_onboarding",
+        });
+
+        return accountLink.url;
+    } catch (e) {
+        console.log(`error while  creating the account link`);
+        throw new GraphQLError(e.message);
+
+    }
 };
 
 module.exports = accountLink;
