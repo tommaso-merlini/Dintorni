5a4ff1bafd002076e4eecffd2e9343d8bfb07547
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2022-01-09 15:18:28 +0100

    imports on index.ts in fuctions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,9 @@
 import * as functions from "firebase-functions";
 
+// The Firebase Admin SDK to access Firestore.
+import admin from "firebase-admin";
+admin.initializeApp();
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
